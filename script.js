53551c24add676bd2584d4fcf352e86500896bef
Shoaib._.Rajpot <126506484+shoaib-pk@users.noreply.github.com>
2023-10-07 01:07:45 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const addbutton = document.querySelector("#add");
+
+const UpdateLocalStorage = () => {
+  const textareaData = document.querySelectorAll("textarea");
+  const notes = [];
+  textareaData.forEach((note) => {
+    return notes.push(note.value);
+  });
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+const addNewNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  const htmldata = ` <div class="operation">
+<button class="edit"><i class="fas fa-edit"></i></button>
+<button class="delete"><i class="fas fa-trash-alt"></i></button>
+</div>
+<div class="main ${text ? "" : "hidden"}"></div>
+<textarea class="${text ? "hidden" : ""}"></textarea>`;
+  note.insertAdjacentHTML("afterbegin", htmldata);
+
+  document.body.appendChild(note);
+
+  // get referance
+
+  const editbutton = note.querySelector(".edit");
+  const delbutton = note.querySelector(".delete");
+  const mainDiv = note.querySelector(".main");
+  const textarea = note.querySelector("textarea");
+
+  // deleting node
+  delbutton.addEventListener("click", () => {
+    note.remove();
+    UpdateLocalStorage();
+  });
+
+  textarea.value = text;
+  mainDiv.innerHTML = text;
+  // toggle using edit button
+  editbutton.addEventListener("click", () => {
+    mainDiv.classList.toggle("hidden");
+    textarea.classList.toggle("hidden");
+  });
+
+  textarea.addEventListener("change", (event) => {
+    const value = event.target.value;
+    mainDiv.innerHTML = value;
+
+    UpdateLocalStorage();
+  });
+};
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
+
+addbutton.addEventListener("click", () => addNewNote());
